05540acf1c35995ab48891c6f0699c143044d93a
Rocco Colonna <colonna.rocco2000@gmail.com>
2021-04-29 19:49:34 +0200

    milestone 1

---
+++ js/script.js
@@ -0,0 +1,55 @@
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            films: [],
+            userFilter: '',
+        },
+        methods: {
+ 
+            // Funzione per Cercare i film o le serie nell' api
+            filterFunction() {
+
+                if( this.userFilter == 0 ) {
+                    
+                    // Se non scrive nessun titolo
+                    alert("Non hai ancora cercato nulla")
+                } else{
+
+                    // Altrimenti CERCA nell' api in base al titolo
+                    axios
+                        .get( 'https://api.themoviedb.org/3/search/multi' , {
+
+                            // elementi richiesti dall'api per funzionare
+                            params: {
+
+                                // key (necessari) per fare le chiamate
+                                api_key: '4453624633412f63fe6e3d3d65a3a501',
+
+                                // query (necessaria) per la ricerca dell'utente
+                                query: this.userFilter,
+
+                                // language (opzionale) lingua in cui fare la ricerca 
+                                language: "it-IT"
+                            }
+                        })
+                        
+                        .then(( response ) => {
+                
+                        const result = response.data;                
+                        // console.log( result );
+                        this.films = result.results;
+                        // console.log( this.films );
+
+                    })
+                }
+
+            }
+        },
+        mounted () {
+
+        }
+    }
+)
+
+            // 
